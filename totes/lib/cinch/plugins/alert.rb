59153c2979239e3cb78688e79235a9db789c474c
Slarticus <wodehousefan01@mail.com>
2015-04-09 12:51:51 -0700

    Update alert.rb

--- totes/lib/cinch/plugins/alert.rb
+++ totes/lib/cinch/plugins/alert.rb
@@ -5,12 +5,14 @@ require 'cinch'
 module Cinch::Plugins
   class Alert
     include Cinch::Plugin
+    
     match(/alert (.+)/, method:  :alert)
     def alert(m, string)
       if $admins.include? m.user.nick
         m.reply Format(:red, :bold, :italic, :underline, 'ATTENTION: ' + string.upcase + '!')
       else
       end
+      
     end
   end
 end
