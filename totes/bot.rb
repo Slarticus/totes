b88db5a105da944c6cffd04fadad5fdfdc06591b
Slarticus <Slarticus@users.noreply.github.com>
2015-04-09 00:35:37 -0700

    modified:   totes/bot.rb 	deleted:    totes/dbs/placeholder.db 	modified:   totes/lib/cinch/plugins/alert.rb 	new file:   totes/lib/cinch/plugins/selfietrain.rb

--- totes/bot.rb
+++ totes/bot.rb
@@ -1,1 +1,29 @@
 
+require 'cinch'
+require 'cinch/plugins/identify'
+require_relative "lib/cinch/plugins/selfietrain"
+require_relative "lib/cinch/plugins/alert"
+
+
+$admins = ['admin 1', 'admin 2']
+
+# botstuff
+bot = Cinch::Bot.new do
+  configure do |c|
+    # plugins
+    c.plugins.plugins = [Cinch::Plugins::Identify, Cinch::Plugins::SelfieTrain, Cinch::Plugins::Alert]
+    c.plugins.options[Cinch::Plugins::Identify] = {
+      username: 'NICK',
+      password: 'PASSWORD',
+      type: :nickserv
+    }
+    c.server = 'IRC SERVER'
+    c.nick = 'NICK'
+    c.channels = ['#CHANNEL 1', '#CHANNEL 2']
+    c.realname = 'NICK'
+    c.user = 'NICK'
+  end
+  # !
+end
+
+bot.start
--- totes/lib/cinch/plugins/alert.rb
+++ totes/lib/cinch/plugins/alert.rb
@@ -1,1 +1,17 @@
+# lib/cinch/plugins/alert.rb
+
+require 'cinch'
+
+module Cinch::Plugins
+  class Alert
+    include Cinch::Plugin
+    match(/alert (.+)/, method:  :alert)
+    def alert(m, string)
+      if $admins.include? m.user.nick
+        m.reply Format(:red, :bold, :italic, :underline, 'ATTENTION: ' + string.upcase + '!')
+      else
+      end
+    end
+  end
+end
 
