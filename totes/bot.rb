6227897fa93eb973b61179f6fa555e2c8420ff7d
Slarticus <Slarticus@users.noreply.github.com>
2015-04-14 18:26:53 -0700

    modified:   bot.rb 	renamed:    lib/cinch/plugins/hugs.rb -> lib/cinch/plugins/OLD_BAD_NO/hugs.rb 	renamed:    lib/cinch/plugins/karma.rb -> lib/cinch/plugins/OLD_BAD_NO/karma.rb 	renamed:    lib/cinch/plugins/selfietrain.rb -> lib/cinch/plugins/OLD_BAD_NO/selfietrain.rb 	new file:   lib/cinch/plugins/uno.rb

--- totes/bot.rb
+++ totes/bot.rb
@@ -5,6 +5,7 @@ require_relative "lib/cinch/plugins/selfietrain"
 require_relative "lib/cinch/plugins/alert"
 require_relative "lib/cinch/plugins/hugs"
 require_relative "lib/cinch/plugins/karma"
+require_relative "lib/cinch/plugins/uno"
 # require_relative "lib/cinch/plugin/PLUGIN GOES HERE"
 
 $admins = ['admin 1', 'admin 2']
@@ -12,7 +13,7 @@ $admins = ['admin 1', 'admin 2']
 # botstuff
 bot = Cinch::Bot.new do
   configure do |c|
-    c.plugins.plugins = [Cinch::Plugins::Identify, Cinch::Plugins::SelfieTrain, Cinch::Plugins::Alert, Cinch::Plugins::Hugs, Cinch::Plugins::Karma] # plugins
+    c.plugins.plugins = [Cinch::Plugins::Identify, Cinch::Plugins::SelfieTrain, Cinch::Plugins::Alert, Cinch::Plugins::Hugs, Cinch::Plugins::Karma, Cinch::Plugins::Uno] # plugins
     c.plugins.prefix = /^./
     c.plugins.options[Cinch::Plugins::Identify] = {
       username: 'NICK',
